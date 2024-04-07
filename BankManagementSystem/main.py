f6cb1790adce90d808cb77b108f3c93bf8fa9670
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2024-04-07 09:33:10 +0530

    Update main.py

--- BankManagementSystem/main.py
+++ BankManagementSystem/main.py
@@ -6,7 +6,7 @@ from mysql.connector import Error
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
+            host="remote",
             user="root",
             password="tanvi@123",
             database="bank_db"
